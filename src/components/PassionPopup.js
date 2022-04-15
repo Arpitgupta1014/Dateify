f1000e5b00c09f645795836e8bcb8adacb15124a
Arpit Gupta <arpitgupta1014@gmail.com>
2022-04-16 01:35:07 +0530

    Update PassionPopup.js

--- src/components/PassionPopup.js
+++ src/components/PassionPopup.js
@@ -8,19 +8,20 @@ function PassionPopup(props) {
   const passionType = [
     "coffee" , "Gamer" , "Fishing" , "Fashion" , "Swimming" , "Ludo" , "Vlogging","Travel" , "Hip hop" , "Art" , "Dancing" , "Comedy" , "Politics" , "Films" , "Poetry" , "k-pop" , "Dog lover" , "Climbing" , "Sports" , "Coocking" , "Cycling" , "Craft beer"
   ]
-  
+  //alert('You have to select 5 things which you like!!');
   if(!props.open) return null;
    return ReactDOM.createPortal(
         <div className="overlay coloumwise">
+           <h4 style={{"color" : "white" , "fontSize" : "20px" , "marginBottom" : "7px" , "fontFamily": "cursive"}}>You have to select 5 passion</h4>
             <div className="inputPassion">
                {
                    passionType.map((obj,index)=>{
                        return(
                         <h4 onClick={()=>{
                             return(
-                               props.passion(passionType[index])
+                              props.passion(passionType[index])
                             );
-                        }}>{passionType[index]}</h4> 
+                        }} >{passionType[index]}</h4> 
                        );
                    })
                }
