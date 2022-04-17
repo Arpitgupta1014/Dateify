2db4c6a5809280a28c2779476bbf9cb63ca1c834
Arpit Gupta <arpitgupta1014@gmail.com>
2022-04-17 22:39:39 +0530

    Update MessageRouter.js

--- backend/Routes/MessageRouter.js
+++ backend/Routes/MessageRouter.js
@@ -1,8 +1,20 @@
-const {addMessage,getAllMessage} = require("../Controllers/MessageControllers");
+const { register, googlelogin,login,editProfile,uploadPic,deletePost,userFeeds,getimage,deleteUsr,AddlikedUser} = require("../Controllers/AuthControllers");
+const {checkUser} = require("../Middlewares/AuthMiddlewares");
 
 const router = require("express").Router();
 
-router.post("/addmsg",addMessage);
-router.post("/getmsg",getAllMessage);
+router.post("/",checkUser);
+
+router.post("/register",register);
+// router.post('/account-activation',accountActivation);
+router.post('/googlelogin',googlelogin);
+router.post("/login",login);
+router.post('/editprofile',editProfile);
+router.post('/uploadpic',uploadPic);
+router.post('/delete',deletePost);
+router.post('/feeds',userFeeds);
+router.post('/getimage',getimage);
+router.post('/deleteUsr',deleteUsr);
+router.post('/AddlikedUser',AddlikedUser);
 
 module.exports = router;
